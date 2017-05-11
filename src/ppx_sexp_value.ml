2c7208ce2c56d20888cd81547030c096f7a3777f
Thomas Refis <thomas.refis@gmail.com>
2017-05-11 16:25:49 +0100

    v0.9.114.44+120

--- src/ppx_sexp_value.ml
+++ src/ppx_sexp_value.ml
@@ -7,8 +7,6 @@ let omit_nil =
     Ast_pattern.(pstr nil)
     ()
 
-let allow_deprecated_syntax = ref false
-
 let sexp_atom ~loc x = [%expr Sexplib.Sexp.Atom [%e x]]
 let sexp_list ~loc x = [%expr Sexplib.Sexp.List [%e x]]
 
@@ -180,88 +178,12 @@ and sexp_of_record ~loc fields =
        wrap_sexp_if_present (omittable_sexp_of_expr e) ~f:k))
 ;;
 
-(* Deprecated since 2015-12. *)
-module Deprecated = struct
-  let hint =
-    "See this page for details:\n\
-     https://github.com/janestreet/ppx_sexp_value"
-
-  let rewrite_arg e =
-    let loc = e.pexp_loc in
-    match e.pexp_desc with
-    | Pexp_constraint _ -> eapply ~loc (evar ~loc "~~") [e]
-    | _ -> e
-  ;;
-
-  let add_warning e msg =
-    let attr = attribute_of_warning e.pexp_loc msg in
-    { e with pexp_attributes = attr :: e.pexp_attributes }
-  ;;
-
-  let rewrite_expr expr =
-    let loc = expr.pexp_loc in
-    match expr.pexp_desc with
-    (* Don't misinterpret [%sexp ~~(e : t)] for the deprecated application syntax. *)
-    | Pexp_apply ({ pexp_desc = Pexp_ident { txt = Lident "~~"; _}; _ }, _) -> expr
-    | Pexp_apply (f, (_ :: _ as args))
-      when List.for_all args ~f:(fun (lab, _) -> match lab with Nolabel -> true | _ -> false) ->
-      let el = List.map (f :: List.map args ~f:snd) ~f:rewrite_arg in
-      let e = pexp_tuple ~loc el in
-      if !allow_deprecated_syntax then
-        e
-      else
-        add_warning e
-          ("ppx_sexp_value: the application syntax is deprecated.\n" ^ hint)
-    | _ -> expr
-  ;;
-
-  let add_deprecated e =
-    if !allow_deprecated_syntax then
-      e
-    else
-      add_warning e
-        ("ppx_sexp_value: deprecated extension, use [%sexp] instead.\n" ^ hint)
-  ;;
-
-  let expand_structural_sexp ~loc:_ ~path:_ e =
-    add_deprecated (sexp_of_expr (rewrite_expr e))
-  ;;
-
-  let expand_error func_name ~loc:_ ~path:_ loc e1 e2 =
-    add_deprecated
-      (eapply ~loc (evar ~loc func_name)
-         [e1; [%expr ()]; [%expr fun () -> [%e sexp_of_expr e2]]])
-  ;;
-end
-
-let expand ~loc ~path e =
-  if !allow_deprecated_syntax then
-    Deprecated.expand_structural_sexp ~loc ~path e
-  else
-    (* Rewrite to make sure we get the warning and not an error *)
-    let e = Deprecated.rewrite_expr e in
-    sexp_of_expr e
-;;
-
-let extensions =
-  let one_expr  =
-    Ast_pattern.(pstr (pstr_eval __ nil ^:: nil))
-  and two_exprs =
-    Ast_pattern.(pstr (pstr_eval (pexp_loc __ (pexp_apply __ ((no_label __) ^:: nil))) nil
-                       ^:: nil))
-  in
-  let declare name patt k =
-    Extension.declare name Extension.Context.expression patt k
-  in
-  [ declare "sexp"                  one_expr  expand
-  ; declare "structural_sexp"       one_expr   Deprecated.expand_structural_sexp
-  ; declare "raise_structural_sexp" two_exprs (Deprecated.expand_error "Error.failwiths")
-  ; declare "structural_error"      two_exprs (Deprecated.expand_error "Error.create"   )
-  ; declare "structural_or_error"   two_exprs (Deprecated.expand_error "Or_error.error" )
-  ]
-;;
-
 let () =
   Ppx_driver.register_transformation "sexp_value"
-    ~extensions
+    ~extensions:[
+      Extension.declare "sexp"
+        Extension.Context.expression
+        Ast_pattern.(pstr (pstr_eval __ nil ^:: nil))
+        (fun ~loc:_ ~path:_ e -> sexp_of_expr e)
+    ]
 ;;
