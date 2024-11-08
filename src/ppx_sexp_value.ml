ed7f78632a6d975a47bbc31a62322d9380c0a30d
public-release <as-public-release@janestreet.com>
2024-11-08 22:02:20 +0000

    v0.18~preview.130.04+450

--- src/ppx_sexp_value.ml
+++ src/ppx_sexp_value.ml
@@ -22,26 +22,31 @@ let sexp_atom ~loc x = [%expr Ppx_sexp_conv_lib.Sexp.Atom [%e x]]
 let sexp_list ~loc x = [%expr Ppx_sexp_conv_lib.Sexp.List [%e x]]
 
 let rec list_and_tail_of_ast_list rev_el e =
-  match e.pexp_desc with
-  | Pexp_construct
-      ({ txt = Lident "::"; _ }, Some { pexp_desc = Pexp_tuple [ hd; tl ]; _ }) ->
-    list_and_tail_of_ast_list (hd :: rev_el) tl
+  match Ppxlib_jane.Shim.Expression_desc.of_parsetree e.pexp_desc ~loc:e.pexp_loc with
+  | Pexp_construct ({ txt = Lident "::"; _ }, Some { pexp_desc; pexp_loc; _ }) ->
+    (match Ppxlib_jane.Shim.Expression_desc.of_parsetree pexp_desc ~loc:pexp_loc with
+     | Pexp_tuple [ (None, hd); (None, tl) ] ->
+       list_and_tail_of_ast_list (hd :: rev_el) tl
+     | _ -> List.rev rev_el, Some e)
   | Pexp_construct ({ txt = Lident "[]"; _ }, None) -> List.rev rev_el, None
   | _ -> List.rev rev_el, Some e
 ;;
 
 let sexp_of_constant ~loc const =
-  let f typ =
+  let mk typ const =
     eapply
       ~loc
       (evar ~loc ("Ppx_sexp_conv_lib.Conv.sexp_of_" ^ typ))
       [ pexp_constant ~loc const ]
   in
-  match const with
+  let f typ = mk typ const in
+  match Ppxlib_jane.Shim.Constant.of_parsetree const with
   | Pconst_integer _ -> f "int"
   | Pconst_char _ -> f "char"
   | Pconst_string _ -> f "string"
   | Pconst_float _ -> f "float"
+  | Pconst_unboxed_float (x, c) -> mk "float" (Pconst_float (x, c))
+  | Pconst_unboxed_integer (x, c) -> mk "int" (Pconst_integer (x, Some c))
 ;;
 
 type omittable_sexp =
@@ -78,6 +83,15 @@ let sexp_of_constraint ~loc expr ctyp =
      | None -> Present expr)
 ;;
 
+let is_list_construction : Ppxlib_jane.Shim.Expression_desc.t -> bool = function
+  | Pexp_construct ({ txt = Lident "[]"; _ }, None) -> true
+  | Pexp_construct ({ txt = Lident "::"; _ }, Some { pexp_desc; pexp_loc; _ }) ->
+    (match Ppxlib_jane.Shim.Expression_desc.of_parsetree pexp_desc ~loc:pexp_loc with
+     | Pexp_tuple [ (None, _); (None, _) ] -> true
+     | _ -> false)
+  | _ -> false
+;;
+
 let rec sexp_of_expr expr =
   match omittable_sexp_of_expr expr with
   | Present v -> v
@@ -90,7 +104,9 @@ and omittable_sexp_of_expr expr =
   let loc = { expr.pexp_loc with loc_ghost = true } in
   wrap_sexp_if_present
     ~f:(fun new_expr -> { new_expr with pexp_attributes = expr.pexp_attributes })
-    (match expr.pexp_desc with
+    (match
+       Ppxlib_jane.Shim.Expression_desc.of_parsetree ~loc:expr.pexp_loc expr.pexp_desc
+     with
      | Pexp_ifthenelse (e1, e2, e3) ->
        Present
          { expr with
@@ -102,10 +118,8 @@ and omittable_sexp_of_expr expr =
                  | None -> None
                  | Some e -> Some (sexp_of_expr e) )
          }
-     | Pexp_constraint (expr, ctyp) -> sexp_of_constraint ~loc expr ctyp
-     | Pexp_construct ({ txt = Lident "[]"; _ }, None)
-     | Pexp_construct
-         ({ txt = Lident "::"; _ }, Some { pexp_desc = Pexp_tuple [ _; _ ]; _ }) ->
+     | Pexp_constraint (expr, Some ctyp, _) -> sexp_of_constraint ~loc expr ctyp
+     | expr_desc when is_list_construction expr_desc ->
        let el, tl = list_and_tail_of_ast_list [] expr in
        let el = List.map el ~f:omittable_sexp_of_expr in
        let tl =
@@ -132,18 +146,28 @@ and omittable_sexp_of_expr expr =
          sexp_list ~loc (elist ~loc [ sexp_atom ~loc (estring ~loc constr); hole ])
        in
        wrap_sexp_if_present (omittable_sexp_of_expr arg) ~f:k
-     | Pexp_tuple el ->
-       let el = List.map el ~f:omittable_sexp_of_expr in
-       Present (sexp_of_omittable_sexp_list loc el ~tl:(elist ~loc []))
+     | Pexp_tuple labeled_el ->
+       (match Ppxlib_jane.as_unlabeled_tuple labeled_el with
+        | Some el ->
+          let el = List.map el ~f:omittable_sexp_of_expr in
+          Present (sexp_of_omittable_sexp_list loc el ~tl:(elist ~loc []))
+        | None ->
+          Location.raise_errorf ~loc "ppx_sexp_value: labeled tuples are unsupported")
      | Pexp_record (fields, None) -> Present (sexp_of_record ~loc fields)
      | Pexp_apply
          ( { pexp_desc = Pexp_ident { txt = Lident "~~"; _ }; _ }
-         , [ (Nolabel, { pexp_desc = Pexp_constraint (expr, ctyp); _ }) ] ) ->
-       let expr_str = Pprintast.string_of_expression expr in
-       let k hole =
-         sexp_list ~loc (elist ~loc [ sexp_atom ~loc (estring ~loc expr_str); hole ])
-       in
-       wrap_sexp_if_present (sexp_of_constraint ~loc expr ctyp) ~f:k
+         , [ (Nolabel, { pexp_desc = inner_desc; pexp_loc = inner_loc; _ }) ] ) ->
+       (match Ppxlib_jane.Shim.Expression_desc.of_parsetree ~loc:inner_loc inner_desc with
+        | Pexp_constraint (expr, Some ctyp, _) ->
+          let expr_str = Pprintast.string_of_expression expr in
+          let k hole =
+            sexp_list ~loc (elist ~loc [ sexp_atom ~loc (estring ~loc expr_str); hole ])
+          in
+          wrap_sexp_if_present (sexp_of_constraint ~loc expr ctyp) ~f:k
+        | _ ->
+          Location.raise_errorf
+            ~loc
+            "ppx_sexp_value: don't know how to handle this construct")
      | _ ->
        Location.raise_errorf
          ~loc
@@ -175,12 +199,18 @@ and sexp_of_record ~loc fields =
     ~tl:(elist ~loc [])
     (List.map fields ~f:(fun (id, e) ->
        let e =
-         match e.pexp_desc with
-         | Pexp_constraint (e', c)
+         match
+           Ppxlib_jane.Shim.Expression_desc.of_parsetree ~loc:e.pexp_loc e.pexp_desc
+         with
+         | Pexp_constraint (e', Some c, modes)
            when Location.compare_pos id.loc.loc_start e.pexp_loc.loc_start = 0
                 && Location.compare e.pexp_loc e'.pexp_loc = 0 ->
            (* { foo : int }  *)
-           { e with pexp_desc = Pexp_constraint ({ e' with pexp_loc = id.loc }, c) }
+           { e with
+             pexp_desc =
+               Pexp_constraint ({ e' with pexp_loc = id.loc }, Some c, modes)
+               |> Ppxlib_jane.Shim.Expression_desc.to_parsetree
+           }
          | _ -> e
        in
        let loc = { id.loc with loc_end = e.pexp_loc.loc_end; loc_ghost = true } in
