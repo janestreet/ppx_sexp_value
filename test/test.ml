26475dccdf7358f6083c43edf75790f283f8c610
public-release <as-public-release@janestreet.com>
2025-05-09 21:16:33 +0100

    v0.18~preview.130.31+242

--- test/test.ml
+++ test/test.ml
@@ -231,3 +231,16 @@ module%test [@name "[%lazy_sexp] output"] _ = struct
         , ~~(None : (string option[@sexp.option]))]
   ;;
 end
+
+let%test_unit "[%sexp] works with ppx_template" =
+  let open%template struct
+    [@@@kind.default k = (bits64, value)]
+
+    type 'a t = T of 'a [@@deriving sexp_of]
+    type 'a not_a_t = Not_t of 'a [@@deriving sexp_of]
+  end in
+  [%test_result: Sexp.t]
+    ~expect:[%sexp (T 1 : int t), (Not_t 2 : int not_a_t)]
+    [%sexp
+      (T 1L : (Int64_u.t t[@kind bits64])), (Not_t 2L : (Int64_u.t not_a_t[@kind bits64]))]
+;;
