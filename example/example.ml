0a57ce10b2a60305f2c282064fffccb822449415
public-release <as-public-release@janestreet.com>
2026-07-10 17:39:00 +0100

    v0.18~preview.130.106+341

--- example/example.ml
+++ example/example.ml
@@ -20,20 +20,14 @@ let _recomended_use_for_errors ~tmpfile ~dst =
 ;;
 
 let _stack_allocated () = exclave_
-  let elems =
-    Local_iterators_to_be_replaced.List.init_local 10 ~f:(fun x -> exclave_
-      string_of_int x)
-  in
+  let elems = (List.init [@alloc stack]) 10 ~f:(fun x -> exclave_ string_of_int x) in
   [%sexp__stack (elems : string list)]
 ;;
 
 (* using ppx template *)
 
 let%template[@alloc a = stack] _stack_allocated () =
-  (let elems =
-     Local_iterators_to_be_replaced.List.init_local 10 ~f:(fun x -> exclave_
-       string_of_int x)
-   in
+  (let elems = (List.init [@alloc stack]) 10 ~f:(fun x -> exclave_ string_of_int x) in
    [%sexp (elems : string list)] [@alloc a])
   [@exclave_if_stack a]
 ;;
